878344ee26a0324cb69af7298f877486fe2b7510
Oleksandr Siaskyi <36467919+Alex9523@users.noreply.github.com>
2020-08-12 01:08:27 +0300

    first commite
    first commite

---
+++ precache-manifest.90d7db26f68705fe24c17fdab0c9c055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bd0433ae30b1ba107b49b6ef534566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b96c4c85280c81d20f8",
+    "url": "/static/css/main.6a22159c.chunk.css"
+  },
+  {
+    "revision": "d063ab54ab956a240fc6",
+    "url": "/static/js/2.2932cace.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2932cace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b96c4c85280c81d20f8",
+    "url": "/static/js/main.1b18763f.chunk.js"
+  },
+  {
+    "revision": "f506de2f2fb7e3e91040",
+    "url": "/static/js/runtime-main.74f46bee.js"
+  }
+]);
